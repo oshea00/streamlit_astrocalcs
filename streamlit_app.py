bd22907a516e4f184e90d74ce82e095be77d8a04
Mike OShea <oshea00@gmail.com>
2023-08-13 11:39:22 -0700

    custom date entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import astrocalcs
 from datetime import date, time
+import re
+
+def getInt(str):
+  i = 0
+  match = re.search("\-{0,1}[\d]+",str)
+  if match:
+    i = int(match[0])
+  return i
 
 st.title('Astrocalcs')
 
@@ -11,12 +19,42 @@ with easter:
   m, d = astrocalcs.easter_date(year)
   month = "April" if m == 4 else "March"
   st.write(f"Easter Sunday {month} {d}, {year}")
+  with st.expander("See explanation"):
+    st.write("Easter day, the date to which such movable feasts as Whitsun and Trinity Sunday "
+             "are fixed, is usually the first Sunday after the fourteenth day after the first new Moon after March 21st. "
+             "The calculation here devised in 1876 comes from Butcher's Ecclesiastical Calender, "
+             "and is valid for all years in the Gregorian calendar from 1583 onwards."
+             )
 
 with julian:
-  gdate = st.date_input("Date",date.today(),date(1582,10,15),format="MM/DD/YYYY")
+  c1, c2, c3 = st.columns(3,gap="small")
+  with c1:
+    gyear = st.text_input("Year",str(date.today().year))
+  with c2:
+    gmon = st.text_input("Month",str(date.today().month))
+  with c3:
+    gday = st.text_input("Day",str(date.today().day))
+  isBce = st.checkbox("Is BCE")
   gtime = st.time_input("Time UTC",value=time(0,0),step=300)
-  julian = astrocalcs.julianDate(gdate.year,gdate.month,gdate.day,gtime.hour,gtime.minute,gtime.second)
-  mjd = astrocalcs.modifiedJulianDate(gdate.year,gdate.month,gdate.day,gtime.hour,gtime.minute,gtime.second)
+
+  intYear = getInt(gyear)
+  intMonth = getInt(gmon)
+  intDay = getInt(gday)
+  
+  if intYear <= 0:
+    st.warning('Year must be positive')
+    st.stop()
+
+  if isBce:
+    intYear = -(intYear-1)
+
+  if intYear < -4712:
+    st.warning('Year cannot be before 4713 BCE')
+    st.stop()
+
+
+  julian = astrocalcs.julianDate(intYear,intMonth,intDay,gtime.hour,gtime.minute,gtime.second)
+  mjd = astrocalcs.modifiedJulianDate(intYear,intMonth,intDay,gtime.hour,gtime.minute,gtime.second)
   st.write(f"Julian date {julian}")
   st.write(f"Modified Julian date (MJD) {mjd}")
 

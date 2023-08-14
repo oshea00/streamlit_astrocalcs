04db16b5e42730d64a26ba43abe211affe5f641c
Mike OShea <oshea00@gmail.com>
2023-08-13 22:49:41 -0700

    better year check

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,16 @@ easter, julian, timeref, astroquery = st.tabs(["Easter Sunday","Julian Dates","T
 
 with easter:
   year = st.text_input("Year",placeholder="Enter a year >= 1583")
-  if (len(year)>0):
-    if int(year)<1583:
-      st.warning("Only years >= 1583 will work!")
-      st.stop()
-    m, d = astrocalcs.easter_date(int(year))
-    month = "April" if m == 4 else "March"
-    st.write(f"Easter Sunday {month} {d}, {year}")
+  try:
+    if (len(year)>0):
+      if int(year)<1583:
+        st.warning("Only years >= 1583 will work!")
+        st.stop()
+      m, d = astrocalcs.easter_date(int(year))
+      month = "April" if m == 4 else "March"
+      st.write(f"Easter Sunday {month} {d}, {year}")
+  except:
+        st.warning("Only numeric years work!")
   with st.expander("See explanation"):
     st.write("Easter day, the date to which such movable feasts as Whitsun and Trinity Sunday "
             "are fixed, is usually the first Sunday after the fourteenth day after the first new Moon after March 21st. "
